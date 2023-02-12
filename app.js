e96c3dc843d61bd0f032ae89b06d266bba458d08
Meshak Otieno <meshakotieno343@gmail.com>
2023-02-12 16:52:53 +0300

    initialized app using nodejs express

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const blogRouter = require("./routes/BlogRoutes");
+ 
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+//middleware
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to meshoblog application." });
+});
+ 
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+ 
+module.exports = app;
